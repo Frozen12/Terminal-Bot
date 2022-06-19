ec9038aee00c2db97c83376c954a6b49f28a90b1
Frozen12 <31291110+Frozen12@users.noreply.github.com>
2022-06-19 18:23:45 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,5 +16,5 @@ if [[ -n $JSON_FILE_URL ]]; then
     curl -o$HOME/.gdrive/token_v2.json "$JSON_FILE_URL"
 
 fi
-
+# start WebDAV server & stat the Telegram bot
 npx webdav-cli --host 0.0.0.0 --port $PORT --directory --autoIndex --username 4a37zccpeelizhk7 --password $PASSWORD --path /app/ & python3 bot.py
